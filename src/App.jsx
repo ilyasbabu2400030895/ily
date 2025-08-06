e2ae4a5091d4ac9e026c8430891c604123e03845
ilyasbabu2400030895 <2400030895@kluniversity.in>
2025-08-06 08:45:19 +0530

    Add image grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,18 @@
-// Importing React library and Component class
-import React, { Component } from 'react';
-// Importing external CSS file to style the component
 import './App.css';
 
-class App extends Component {
-  // The render method returns the JSX (UI structure) to be displayed
-  render() {
-    return (
-      <>
-        {/* Header section containing a logo */}
-        <header>
-          <div className='logo'>My Projects</div>
-          {/* .logo class styles the text to look like a bold site/project title */}
-        </header>
+function App() {
+  const images = Array.from({ length: 9 }, (_, i) => `/${i + 1}.jpeg`);
 
-        {/* Main content section displaying project cards in a grid layout */}
-        <section>
-          {/* Each card displays an image and a label/title below it */}
-          <div className='card'>
-            <img src='/1.jpeg' alt='' />
-            <label>Project One</label>
-          </div>
-
-          <div className='card'>
-            <img src='/2.jpeg' alt='' />
-            <label>Project Two</label>
-          </div>
-
-          <div className='card'>
-            <img src='/3.jpeg' alt='' />
-            <label>Project Three</label>
-          </div>
-
-          <div className='card'>
-            <img src='/4.jpeg' alt='' />
-            <label>Project Four</label>
-          </div>
-
-          <div className='card'>
-            <img src='/5.jpeg' alt='' />
-            <label>Project Five</label>
-          </div>
-
-          <div className='card'>
-            <img src='/6.jpeg' alt='' />
-            <label>Project Six</label>
-          </div>
-        </section>
-
-        {/* Footer section with copyright notice */}
-        <footer>
-          Copyright @ 2025. All rights reserved.
-        </footer>
-      </>
-    );
-  }
+  return (
+    <div className="container">
+      <h1>Image Grid</h1>
+      <div className="grid">
+        {images.map((src, index) => (
+          <img key={index} src={src} alt={`img-${index + 1}`} className="grid-image" />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-// Exporting the App component so it can be used in other parts of the application
 export default App;
---
+++ src/test.jsx
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <h1>u clicked me</h1>
+    </body>
+</html>
